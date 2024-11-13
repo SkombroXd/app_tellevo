68069e4a56d0c47c34c764dedf034f2f32d6bb61
Kevin <kevin.kf.20xd@gmail.com>
2024-11-13 19:15:30 -0300

    Mapa
    Se integró mapa y implementó Geolocation

--- myApp/src/app/pages/homec/homec.page.ts
+++ myApp/src/app/pages/homec/homec.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { Geolocation } from '@capacitor/geolocation';
+
 
 @Component({
   selector: 'app-homec',
   templateUrl: './homec.page.html',
   styleUrls: ['./homec.page.scss'],
 })
+
 export class HomecPage implements OnInit {
+  map!: mapboxgl.Map;
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  async ngOnInit() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    const lat = coordinates.coords.latitude;
+    const lon = coordinates.coords.longitude;
 
+    (mapboxgl as any).accessToken = 'pk.eyJ1Ijoia2VmaWVycm8iLCJhIjoiY20zZ2NoYm91MDJ3cDJxcHRseGZxZnpmdyJ9.EZiJXVqhIfThpB9n3C308g';
+    this.map = new mapboxgl.Map({
+      container: 'map', // ID del contenedor
+      style: 'mapbox://styles/mapbox/streets-v11', // Estilo de mapa de Mapbox
+      center: [lon, lat], // Coordenadas de inicio
+      zoom: 9 // Nivel de zoom inicial
+    });
+  }
 }
