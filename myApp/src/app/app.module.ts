a51b06347413a184a07ffe74ee76a9fb0d4c875e
Kevin <kevin.kf.20xd@gmail.com>
2024-11-15 08:37:15 -0300

    Guards
    Se implementaron Guards

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
+import { AuthService } from './services/auth.service'; // Asegúrate de que la ruta sea correcta
+import { AuthGuard } from './services/auth.guard'; // Importa el guard
+
 import { TabsComponent } from './components/tabs/tabs.component';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
@@ -16,10 +19,11 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { TabscComponent } from './components/tabsc/tabsc.component';
 import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [AppComponent, TabsComponent, TabscComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig),AngularFireAuthModule,AngularFireDatabaseModule,HttpClientModule,AngularFirestoreModule,],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },AuthService, AuthGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
